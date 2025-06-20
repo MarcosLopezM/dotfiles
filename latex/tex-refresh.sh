a82d23792152f160231de31d6e5671c30348e324
MarcosLM <marcoslm@ciencias.unam.mx>
2025-06-20 17:43:30 -0600

    Enable backslash escapes

--- latex/tex-refresh.sh
+++ latex/tex-refresh.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-echo "\e[34mRefreshin TeX and font databases...\e[0m"
+echo -e "\e[34mRefreshin TeX and font databases...\e[0m"
 
 sudo env PATH="$PATH" mktexlsr
 luaotfload-tool -fu
 fc-cache -fv
 
-echo "\e[32mTeX and font databases refreshed successfully.\e[0m"
+echo -e "\e[32mTeX and font databases refreshed successfully.\e[0m"
